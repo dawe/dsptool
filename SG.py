940e152c99e5cfc8e81594ceccd5b12a9d3dd827
shahramsaghaei <48684010+shahramsaghaei@users.noreply.github.com>
2019-08-16 17:47:28 +0200

    Update SG.py

--- SG.py
+++ SG.py
@@ -40,14 +40,14 @@ def segmentation(chr,str,end,step):
     sg_value = sg_input.values(chr, str, end, numpy=True)[::step]
     ZeroCrossList = Sobel_filters(sg_value)     #using ndimage
     ListofMaxima, ListofMinima, ListofPlateau = Maxima(ZeroCrossList)
-    MergedList = Merge(ListofMaxima,ListofPlateau)
+    #MergedList = Merge(ListofMaxima,ListofPlateau)
     j,k=0,0
-    if MergedList:
-        for i in MergedList:
+    if ListofMaxima:
+        for i in ListofMaxima:
             if sg_step > i-j:
                 pass
             else:
-                k = i*step
+                k = i*step+str
                 intensity =float(sg_value[i])
                 j = i
                 sg_output.addEntries(chr, k, values=[intensity], span=step, step=step)
@@ -64,12 +64,17 @@ parser.add_argument("-p", "--peak", help="Define the name and the location of th
 parser.add_argument("-S", "--step", type=int, help="The step size that denoising process applied by, the default step size is 50bps.", default=50)
 parser.add_argument("-r", "--region", help="A single section of input file could be defined as chromosomename:startindex:endindex")
 parser.add_argument("-seg", "--segmentation", action='store_true', help="Run segmentation")
+parser.add_argument("-l", "--interval", help="A list of regions could be defined as a BED file format")
+parser.add_argument("-H", "--highresolution", action='store_true', help="Force the program to define step and span equal to 1 to have the highest resolution as possible (One base resolution)")
+
 args = parser.parse_args()
 input = args.output
 output = args.peak
 sg_region = args.region
+sg_interval = args.interval
 sg_step = int(args.step)
-
+if args.highresolution:
+    sg_step, sg_span = 1, 1
 sg_input = bw.open(input)
 sg_output = bw.open(output, "w")
 sg_output.addHeader(list(sg_input.chroms().items()))
@@ -90,6 +95,34 @@ if sg_region:
         sys.stderr.write('Interval definition is incorrect (Larger than the length of the chromosome).\n')
         exit()
     segmentation(sg_name, sg_start, sg_end, sg_step)
+# If the BED file selected
+elif sg_interval:
+    import  tempfile, pybedtools, pathlib
+    bed_file = pathlib.Path(sg_interval)
+    # define a temperory file and put the list of the chromosome name inside it, because the Bedtools does not accept the list as a array
+    tmp = tempfile.NamedTemporaryFile()
+    with open(tmp.name, 'w') as _tmp:
+        for line in chrlist:
+            _tmp.write(str(line+"\n"))
+    # Call temprory file
+    with open(tmp.name) as _tmp:
+        # Check if the Bed file present, continue
+        if bed_file.is_file():
+            try:
+                # Interval data derived from Bed file sorted and merged if their steps overlapped
+                sites = list(pybedtools.BedTool(sg_interval).sort(g = _tmp.name).merge(d=sg_step-1))
+            except:
+                # for bedtools sort Version: v2.26.0
+                sites = list(pybedtools.BedTool(sg_interval).sort(faidx = _tmp.name).merge(d=sg_step-1))
+            # For each interval the value reads and stored in a varible
+            for line in sites:
+                _temp=str(line)
+                L = _temp.strip().split()
+                print(L)
+                segmentation(L[0], int(L[1]), int(L[2]), sg_step)
+        else:
+            sys.stderr.write("File \"" + sg_interval + "\" is not exist. Check the BED file and it\'s path. \n")
+            exit()
 else:
     sg_start=1
     for line in chrlist:
