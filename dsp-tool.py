9bf274196b0f61643388ceaeb2db12e73ab7130a
shahramsaghaei <48684010+shahramsaghaei@users.noreply.github.com>
2019-08-27 18:21:02 +0200

    Update dsp-tool.py

--- dsp-tool.py
+++ dsp-tool.py
@@ -1,5 +1,6 @@
-import sys, time, os
+import sys, time, os, pathlib
 s_time = time.time()
+folder, filename = os.path.split(os.path.abspath(__file__))
 arguments = sys.argv[1:]
 import filter as fl
 if fl.esc:
@@ -8,7 +9,8 @@ arg = ' '.join(arguments)
 if fl.args.segmentation:
     del fl
     del sys.modules["filter"]
-    os.system('python SG.py '+arg)
+    segmentation=str('python '+ folder+ '/SG.py '+arg)
+    os.system(segmentation)
 else:
     pass
 print('Completed in',"%.2f seconds" % (time.time()-s_time))
